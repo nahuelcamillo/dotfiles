ff86bd8b1666e10a207b8e2f5fc2d5192b9a016d
nahuelcamillo <nahuel.camillo@gmail.com>
2023-08-03 15:03:17 -0300

    :wrench: Solution to Natural Scrolling issue
    init.sh update to fix the issue of Natural Scrolling with Magic Trackpad 2

--- programs/init.sh
+++ programs/init.sh
@@ -8,6 +8,19 @@ newgrp input
 
 mkdir -p ~/.config/fusuma        # Create config directory
 
+# Related to gestures, solution to natural scrolling issue with magic trackpad 2.
+# Create file
+sudo nano /etc/X11/xorg.conf.d/90-magic-trackpad.conf
+
+# Insert the following lines and uncomment
+
+# Section "InputClass"
+#     Identifier "Magic Trackpad 2"
+#     MatchProduct "Apple Inc. Magic Trackpad 2"
+#     Driver "libinput"
+#     Option "NaturalScrolling" "true"
+# EndSection
+
 # Virtualization configs
 sudo systemctl enable --now libvirtd
 sudo nano /etc/libvirt/libvirtd.conf
@@ -15,9 +28,4 @@ sudo nano /etc/libvirt/libvirtd.conf
 # Uncomment line "unix_sock_rw_perms = "0770""
 sudo usermod -a -G libvirt $USER
 sudo systemctl restart libvirtd
-
-
-
-
-
 
