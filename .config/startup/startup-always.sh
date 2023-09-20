f8b92e95f593216bd2f828c8ac7e82b581e02192
Nahuel Camillo <nahuel.camillo@gmail.com>
2023-09-20 09:05:30 -0300

    :wrench: Update of i3 config and startup script due to polybar usage.

--- .config/startup/startup-always.sh
+++ .config/startup/startup-always.sh
@@ -6,14 +6,15 @@
 # System
 docker &
 xset -b &
-# xsettingsd & # Used to scale GTK apps when going 2K <=> 4K
+xsettingsd & # Used to scale GTK apps when going 2K <=> 4K
 xfce4-power-manager &
 
 # Apps
 1password --silent &
-$HOME/bin/exterminate nm-applet; nm-applet &
-$HOME/bin/exterminate blueberry; blueberry &
-$HOME/bin/exterminate copyq; copyq &
-$HOME/bin/exterminate fusuma; fusuma &
-$HOME/bin/exterminate flameshot; flameshot &
-$HOME/bin/exterminate sxhkd; mkfifo /tmp/sxhkd.fifo || true; sxhkd -t 10 -s /tmp/sxhkd.fifo &
+$HOME/.bin/exterminate nm-applet; nm-applet &
+$HOME/.bin/exterminate blueberry; blueberry &
+$HOME/.bin/exterminate copyq; copyq &
+$HOME/.bin/exterminate fusuma; fusuma &
+$HOME/.bin/exterminate dunst; dunst &
+$HOME/.bin/exterminate flameshot; flameshot &
+$HOME/.bin/exterminate sxhkd; mkfifo /tmp/sxhkd.fifo || true; sxhkd -t 10 -s /tmp/sxhkd.fifo &
