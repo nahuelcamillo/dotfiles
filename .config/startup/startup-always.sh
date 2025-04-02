25ecfa071f5ca4e98c04c1d2602872d440266341
Nahuel Camillo <nahuel.camillo@gmail.com>
2025-04-02 13:53:12 -0300

    :wrench: startup: change copyq path and removal of xsettingsd

--- .config/startup/startup-always.sh
+++ .config/startup/startup-always.sh
@@ -6,14 +6,14 @@
 # System
 docker &
 xset -b &
-xsettingsd & # Used to scale GTK apps when going 2K <=> 4K
+#xsettingsd & # Used to scale GTK apps when going 2K <=> 4K
 xfce4-power-manager &
 
 # Apps
 1password --silent &
 $HOME/.bin/exterminate nm-applet; nm-applet &
 #$HOME/.bin/exterminate blueberry; blueberry &
-$HOME/.bin/exterminate copyq; copyq &
+$HOME/.bin/exterminate copyq; ~/.local/bin/copyq &
 $HOME/.bin/exterminate fusuma; fusuma &
 $HOME/.bin/exterminate dunst; dunst &
 $HOME/.bin/exterminate flameshot; flameshot &
